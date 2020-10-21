a27e44fd359ee7747103d5492ce5d54cd904753e
Non Existing <0x63ff@gmail.com>
2020-10-21 22:04:32 +0000

    upd

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -4,10 +4,12 @@ for ver in $DOCKER_ARGS; do
 
   echo "Building ${DOCKER_VERS} from "${CI_COMMIT_BRANCH}""
 
+  [[ "${CI_COMMIT_BRANCH}" = "master" ]] && NOT_DUMMY_SSL=true || NOT_DUMMY_SSL=false
+
   if [ "${DOCKER_VERS}" = "core" ]; then
-    docker build --no-cache -t $DOCKER_TEMP:$DOCKER_VERS .
+    docker build --no-cache -t $DOCKER_TEMP:$DOCKER_VERS --build-arg NOT_DUMMY_SSL=$NOT_DUMMY_SSL .
   else
-    docker build -t $DOCKER_TEMP:$DOCKER_VERS --build-arg PHP_VERSION=$DOCKER_VERS --build-arg BUILD_BRANCH=$CI_COMMIT_BRANCH .
+    docker build -t $DOCKER_TEMP:$DOCKER_VERS --build-arg PHP_VERSION=$DOCKER_VERS --build-arg NOT_DUMMY_SSL=$NOT_DUMMY_SSL .
   fi
 
 done
