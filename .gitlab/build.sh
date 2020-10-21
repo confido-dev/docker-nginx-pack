530f535e568e63e42f4e3f5188e406f9747335ab
Mike <inspirium.dev@gmail.com>
2020-10-22 00:46:22 +0300

    test

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -9,7 +9,7 @@ for ver in $DOCKER_ARGS; do
   [[ "${CI_COMMIT_BRANCH}" = "develop" ]] && NOT_DUMMY_SSL="true" || NOT_DUMMY_SSL="false"
 
   if [ "${DOCKER_VERS}" = "core" ]; then
-    docker build --no-cache -t $DOCKER_TEMP:$DOCKER_VERS .
+    docker build --no-cache -t $DOCKER_TEMP:$DOCKER_VERS --build-arg NOT_DUMMY_SSL=$NOT_DUMMY_SSL .
   else
     docker build -t $DOCKER_TEMP:$DOCKER_VERS --build-arg PHP_VERSION=$DOCKER_VERS --build-arg NOT_DUMMY_SSL=$NOT_DUMMY_SSL .
   fi
