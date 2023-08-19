f8c8de4855d7d57beb7242ecd907b0eec3822f9e
confido-dev <inspirium.srv@gmail.com>
2023-08-20 01:13:54 +0300

    realip option

--- corepoint.sh
+++ corepoint.sh
@@ -76,7 +76,7 @@ cat /etc/nginx/conf.d/sources/proxy-headers-basic.conf > /etc/nginx/conf.d/proxy
 # Cleaning XDebug
 rm -f /etc/php/current/fpm/conf.d/20-xdebug.ini
 # RealIP
-if [ -n "${NGINX_REALIP}" ]; then then
+if [ -n "${TRUSTED_PROXIES}" ]; then then
     echo " ---> Enabling NGINX RealIP module"
     cat /etc/nginx/fastcgi.d/sources/realip.conf >> /etc/nginx/fastcgi.d/realip.conf
 fi
