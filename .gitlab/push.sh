b046052a34680edf6aca4000339d8a34bc4506d6
Vigilo Confido <vigilo@confido.dev>
2023-12-02 23:57:15 +0000

    Develop

--- .gitlab/push.sh
+++ .gitlab/push.sh
@@ -7,9 +7,9 @@ for ver in $DOCKER_ARGS; do
   echo "Building final multiarch images for ${DOCKER_VERS}:"
 
   if [ "${DOCKER_VERS}" = "core" ]; then
-    docker buildx build --platform $DOCKER_ARCH --no-cache --tag $DOCKER_TEMP:$DOCKER_VERS --build-arg NOT_DUMMY_SSL=true --push .
+    docker buildx build --platform $DOCKER_ARCH --no-cache --tag $DOCKER_BASE:$DOCKER_VERS --build-arg NOT_DUMMY_SSL=true --push .
   else
-    docker buildx build --platform $DOCKER_ARCH --tag $DOCKER_TEMP:php$DOCKER_VERS --build-arg PHP_VERSION=$DOCKER_VERS --build-arg NOT_DUMMY_SSL=true --push .
+    docker buildx build --platform $DOCKER_ARCH --tag $DOCKER_BASE:php$DOCKER_VERS --build-arg PHP_VERSION=$DOCKER_VERS --build-arg NOT_DUMMY_SSL=true --push .
   fi
 
 done
