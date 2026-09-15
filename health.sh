c4c7c9f87b410ea73c51234ae274a207d3f6aafb
Vigilo Confido <vigilo@confido.dev>
2026-09-16 02:33:33 +0400

    health.sh upd

--- health.sh
+++ health.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 [ "${PHP_VERSION}" != "false" ] && status_page="fpm_ping" || status_page="nginx_status"
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null "http://127.0.0.1:80/${status_page}")
-if [ "$status_code" != "200" ]; then exit 1; fi
 
-exit 0;
+status_code=$(curl --connect-timeout 2 --max-time 5 --silent --show-error \
+                   --output /dev/null --write-out '%{http_code}' \
+                   "http://127.0.0.1:80/$status_page") || exit 1
+
+[ "$status_code" = "200" ]
