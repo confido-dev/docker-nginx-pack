7ad8abf623636c4a6cf399885b1b23f5a8f5c365
Vigilo Confido <vigilo@confido.dev>
2026-09-16 01:58:05 +0400

    xdebug fix

--- corepoint.sh
+++ corepoint.sh
@@ -76,7 +76,8 @@ rm -f /etc/nginx/modules-enabled/50-mod-http-geoip2.conf
 # Resetting proxy_pass headers
 cat /etc/nginx/conf.d/sources/proxy-headers-basic.conf > /etc/nginx/conf.d/proxy-headers.conf
 # Cleaning XDebug
-rm -f /etc/php/current/fpm/conf.d/70-xdebug.ini
+rm -f /etc/php/current/fpm/conf.d/20-xdebug.ini \
+      /etc/php/current/fpm/conf.d/70-xdebug.ini
 # RealIP
 if [ -n "${NGINX_REALIP}" ]; then
     echo " ---> Enabling NGINX RealIP module"
